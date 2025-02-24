f3b78f3fdc6935ce1b61e885f68832e6fdec77fc
Joseph Tiglao <josep.tiglao12@gmail.com>
2025-02-24 10:12:41 +0800

    not finish

--- lib/productList.dart
+++ lib/productList.dart
@@ -2,47 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:labwork/product.dart';
 import 'package:realm/realm.dart';
 
-var config;
-var realm;
-
-// class Config {
-//   Config() {
-//     config = Configuration.local([Product.schema]);
-//     realm = Realm(config);
-//   }
-// }
+class Config {
+  static Realm realmGenerate() {
+    return Realm(Configuration.local([Product.schema]));
+  }
+}
 
 class Products extends ChangeNotifier {
   // final List<Product> _products = [Product("12", "Ink", "12")];
+  Realm realm = Config.realmGenerate();
 
-  Products() {
-    config = Configuration.local([Product.schema]);
-    realm = Realm(config);
-  }
-
-  late RealmResults<Product> _products;
-  //   RealmResults<Product> set setItems (RealmResults<Product> item) {_products;}
-
-  // Products ({this.config, this.realm, this.setItems(RealmResults)})
-
-  // void init() {
-  //   config = Configuration.local([Product.schema]);
-  //   realm = Realm(config);
-  //   _products = realm.all<Product>();
+  // RealmResults<Product> get items {
+  //   var realm = Config.realmGenerate();
+  //   return realm.all<Product>();
   // }
 
-  // Product() {
-  //   config = Configuration.local([Product.schema]);
-  //   realm = Realm(config);
-  //   _products = realm.all<Product>();
-  // }
+  late RealmResults<Product> _products = realm.all<Product>();
 
   RealmResults<Product> get items => _products;
 
   void add(Product p) {
     // _products.add(p);
-
-    realm(() {
+    realm.write(() {
       realm.add(p);
     });
     notifyListeners();
